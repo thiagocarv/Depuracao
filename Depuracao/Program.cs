786874127a03d4f3791194b7051ad40df1173438
Thiago <thiagocarv@uol.com.br>
2012-08-16 21:35:54 -0300

    Adicionei a divisao.

--- Depuracao/Program.cs
+++ Depuracao/Program.cs
@@ -14,12 +14,27 @@ namespace Depuracao
             return n;
         }
 
+        static float Dividir(int n, int d)
+        {
+
+            return n / d;
+        }
+        
         static void Main(string[] args)
         {
             int numero = 0;
             numero = int.Parse(Console.ReadLine());
             Console.WriteLine("O numero escolhido e: {0}", numero);
             numero = Mudar(numero);
+            Console.WriteLine("O numero modificado e: {0}", numero);
+
+            int denominador = 0;
+            denominador = int.Parse(Console.ReadLine());
+            if (denominador == 0)
+                Console.WriteLine("O denominador é zero");
+            else
+            Console.WriteLine("O quociente entre {0} e {1} é: {2}", numero, denominador, Dividir(numero, denominador));
+
             Console.Read();
         }
     }
